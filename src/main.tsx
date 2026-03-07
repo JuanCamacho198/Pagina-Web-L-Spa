37ad6edbc79ab5e5858c2f4a16ea0fcf7538a2ff
Juan_Camacho <jcamachomolina503@gmail.com>
2026-03-07 17:48:13 -0500

    feat: add Vercel Analytics integration to the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";
 import App from "./App";
 import { CartProvider } from "@context/CartContext";
@@ -21,8 +22,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <CartProvider>
         <ToastProvider>
           <App />
+          <Analytics />
         </ToastProvider>
       </CartProvider>
+
     </Auth0Provider>
   </React.StrictMode>
 );
